a20c27cdc54dd7bf9c688bed95cba8cfd03b5ad6
Marc Gueury <marc.gueury@oracle.com>
2023-05-08 12:51:57 +0200

    v1

--- starter/src/terraform/oic.tf
+++ starter/src/terraform/oic.tf
@@ -1,10 +1,9 @@
-resource "oci_integration_integration_instance" "test_integration_instance" {
+resource "oci_integration_integration_instance" "opensearch_oic" {
   #Required
   compartment_id = local.lz_appdev_cmp_ocid
   display_name  = "${var.prefix}-oic"  
+  # OIC GEN 3 
   integration_instance_type = "STANDARDX"
-  shape                     = "DEVELOPMENT"
-  # shape                   = "PRODUCTION"
   is_byol                   = "true"
   message_packs             = "1"
   # idcs_at                   = var.integration_instance_idcs_access_token
