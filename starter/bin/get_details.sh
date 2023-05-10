a0151c31033784f59b3548ed1a9fcc6afab5c9bf
Marc Gueury <marc.gueury@oracle.com>
2023-05-10 23:06:28 +0200

    v1

--- starter/bin/get_details.sh
+++ starter/bin/get_details.sh
@@ -6,15 +6,17 @@ cd $ROOT_DIR
 . ./env.sh
 
 get_attribute_from_tfstate "STREAM_BOOSTRAPSERVER" "opensearch_stream_pool" "kafka_settings[0].bootstrap_servers"
+get_attribute_from_tfstate "STREAM_OCID" "opensearch_stream_pool" "id"
 get_attribute_from_tfstate "TENANCY_NAME" "tenant_details" "name"
 
 echo "COMPARTMENT_OCID=$TF_VAR_compartment_ocid"
 echo "STREAM_BOOSTRAPSERVER=$STREAM_BOOSTRAPSERVER"
-echo "TENANCY_NAME=$TENANCY_NAME"
+# echo "TENANCY_NAME=$TENANCY_NAME"
+echo "STREAM_USERNAME=$TENANCY_NAME/$TF_VAR_username/$STREAM_OCID"
 echo "STREAM_USERNAME=tenancyname/oracleidentitycloudservice/name@domain.com/ocid1.streampool.oc1.eu-frankfurt-1.amaccccccccfsdfsdxfa"
 echo "# OPENSEARCH_USER=opensearch-user"
 echo "# OPENSEARCH_PWD=LiveLab--123"
-echo "##COMPUTE_PUBLIC-IP##"
+echo "COMPUTE_PUBLIC-IP=$COMPUTE_IP"
 echo "##COMPUTE_PRIVATE-KEY##"
 echo "##OPENSEARCH_HOST##"
 
