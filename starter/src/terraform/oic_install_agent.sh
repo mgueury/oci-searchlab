d0a53b0ce6e8faee6bd8aace2dd534a05a2f627f
Marc Gueury <marc.gueury@oracle.com>
2023-06-19 12:16:10 +0200

    v1

--- starter/src/terraform/oic_install_agent.sh
+++ starter/src/terraform/oic_install_agent.sh
@@ -13,38 +13,26 @@ echo OIC_DOMAIN=$OIC_DOMAIN
 # Download the OIC_agent
 # OIC Gen 2
 # curl -X GET  $OIC_HOST/ic/api/integration/v1/agents/binaries/connectivity -u $OCI_USER:$OCI_PASSWORD -o $HOME/oic_connectivity_agent.zip
-curl -X GET https://design.integration.$OIC_DOMAIN/ic/api/integration/v1/agents/binaries/connectivity?integrationInstance=$OIC_NAME -u $OCI_USER:$OCI_PASSWORD -o $HOME/oic_connectivity_agent.zip
-ls -a
+# curl -X GET https://design.integration.$OIC_DOMAIN/ic/api/integration/v1/agents/binaries/connectivity?integrationInstance=$OIC_NAME -u $OCI_USER:$OCI_PASSWORD -o $HOME/oic_connectivity_agent.zip
+# ls -a
+
+# OIC3
+# export OIC_SCOPE=https://xxxxxxx.integration.eu-frankfurt-1.ocp.oraclecloud.com:443/ic/api/
+# export OIC_CLIENT_ID=xxxx
+# export OIC_CLIENT_SECRET=xxxx
+# export IDCS_HOST=idcs-xxx.identity.oraclecloud.com
+export ACCESS_TOKEN=`curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&scope=$OIC_SCOPE" -u "$OIC_CLIENT_ID:$OIC_CLIENT_SECRET" "https://$IDCS_HOST/oauth2/v1/token" | jq -r ".access_token"`
+curl -X GET "https://design.integration.$OIC_DOMAIN/ic/api/integration/v1/agents/binaries/connectivity?integrationInstance=$OIC_NAME" -H "Authorization: Bearer $ACCESS_TOKEN" -o oic_connectivity_agent.zip
+curl -X GET "https://design.integration.$OIC_DOMAIN/ic/api/integration/v1/agentgroups/OPENSEARCH_AGENT_GROUP/configuration?integrationInstance=$OIC_NAME" -H "Authorization: Bearer $ACCESS_TOKEN" -o InstallerProfile.cfg
 
 # Unzip it
 mkdir oic_agent
 cd oic_agent
 unzip ../oic_connectivity_agent.zip
+cp ../InstallerProfile.cfg
 
-
-# Configure it
-mv InstallerProfile.cfg InstallerProfile.orig
-cat > ./InstallerProfile.cfg << EOT
-# Required Parameters
-# oic_URL format should be https://hostname:sslPort
-oic_URL=$OIC_HOST
-agent_GROUP_IDENTIFIER=$AGENT_GROUP
-
-#Optional Parameters
-oic_USER=$OCI_USER
-oic_PASSWORD=$OCI_PASSWORD
-
-# Proxy Parameters
-# proxy_NON_PROXY_HOSTS: a list of hosts that should be reached directly, bypassing the proxy. This is a list of patterns separated by '|'.
-proxy_HOST=
-proxy_PORT=
-proxy_USER=
-proxy_PASSWORD=
-proxy_NON_PROXY_HOSTS=
-EOT
-
-# Install JDK 11
-sudo yum install java-11-openjdk-devel -y
+# Install JDK 17
+sudo yum install java-17-openjdk-devel -y
 
 # Get the SSL certificate of OpenSearch since it is invalid
 echo -n | openssl s_client -connect $OPENSEARCH_HOST:9200 -servername $OPENSEARCH_HOST | openssl x509 > /tmp/opensearch.cert
